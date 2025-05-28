644b173ff302ba7287eada3c2bad345c38c21d59
Asraf Wismaputra <asrafsaputra792@gmail.com>
2025-05-29 00:18:43 +0700

    Initial commit

---
+++ latihan1.php
@@ -0,0 +1,14 @@
+<?php
+
+$Mahasiswa = [
+    "Nama" => "Asraf Wisma Putra",
+    "Nim" => "2217020157",
+    "Mata kuliah" => "integrasi aplikasi perusahaan",
+    "Email" => "asrafsaputra792@gmail.com"
+];
+
+
+$data = json_encode($Mahasiswa);
+echo $data;
+
+?>
